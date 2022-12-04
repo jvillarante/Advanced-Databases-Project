aa55dbf8ed13d8014c2de9b75420d2b9b1e3be12
JVilla1997 <jorge.villavicencio.013@gmail.com>
2022-12-03 18:34:34 -0800

    basic javascript fetch

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-const request = XMLHttpRequest();
-
-request.addEventListener('readystatechange', () => {
-    //console.log(request, request.readyState)
-    if(request.readyState === 4) {
-        console.log(request.responseText)
-    }
-});
-
-let fName = document.getElementById('fname').value;
-
-let submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", (fName)=>{
-    request.open('GET', 'https://csc174-project.herokuapp.com/selectCustomer?customerID=' + fName);
-    request.send();
-});
-
-
-request.open('POST', ' https://csc174-project.herokuapp.com/createCustomer');
-request.send();
-
+fetch('https://csc174-project.herokuapp.com/createCustomer',{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'fName'
+    })
+}).then(res => {
+    return res.json()
+})
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
