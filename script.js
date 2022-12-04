1cddc447ea942503643715bd43a9326d66cd9dfa
Josiah <josiahvillarante@gmail.com>
2022-12-03 18:54:22 -0800

    Added doRequest() function

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ fetch('https://csc174-project.herokuapp.com/createCustomer',{
 })
     .then(data => console.log(data))
     .catch(error => console.log('ERROR'))
+
+async function doRequest() {
+
+    let url = 'https://csc174-project.herokuapp.com/createCustomer';
+    let fName = document.getElementById('fname').value;
+    let data = JSON.stringify({'fname': fName});
+
+    let res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (res.ok) {
+
+        // let text = await res.text();
+        // return text;
+
+        let ret = await res.json();
+        return JSON.parse(ret.data);
+
+    } else {
+        return `HTTP error: ${res.status}`;
+    }
+}
+
+doRequest().then(data => {
+    console.log(data);
+});
