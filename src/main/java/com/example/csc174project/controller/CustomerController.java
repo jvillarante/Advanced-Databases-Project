5504a96b17d2fbd29bed9def575c8d36b8ae7d9a
Josiah <josiahvillarante@gmail.com>
2022-12-03 19:39:54 -0800

    Added CrossOrigin to allow requests from frontend hosted at Netlify

--- src/main/java/com/example/csc174project/controller/CustomerController.java
+++ src/main/java/com/example/csc174project/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = {"https://gleeful-profiterole-1a83ad.netlify.app/"})
 
 public class CustomerController {
 
